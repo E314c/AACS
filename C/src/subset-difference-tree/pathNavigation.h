ac42c149278baab9441bbd21f91796e3c42bb1bf
E314c <e314c.rael@gmail.com>
2019-07-05 08:14:44 +0200

    Improved testing

---
+++ C/src/subset-difference-tree/pathNavigation.h
@@ -0,0 +1,5 @@
+#include "../common/common.h"
+
+uint vMaskFromUv(uint uv);
+int isNodeInSubset(uint deviceNodeNumber, uint uv, uint u_mask);
+int isCorrectDeviceKey(uint keyUv, uint keyUMask, uint subsetUv, uint subsetUMask);
